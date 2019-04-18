c407b58dd78798d28bd0e064bf8b1226f3ddfa8b
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2019-04-18 14:17:54 +0700

    add: scrap imbd nodejs

---
+++ scrap-imbd/app.js
@@ -0,0 +1,7 @@
+const Fx = require('./fx');
+
+const URLS = [
+   'https://www.imdb.com/title/tt0102926/?ref_=nv_sr_1',
+   'https://www.imdb.com/title/tt4633694/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=AFQ79AQGX5JBGNGQCK5A&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_49']
+
+Fx.load(URLS)
---
+++ scrap-imbd/fx.js
@@ -0,0 +1,52 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+const Fx = (() => {
+    return {
+        load: async URLS => {
+            let genres = [];
+            
+            for(let movie of URLS) {
+                const response = await request({
+                    uri: movie,
+                    headers: {
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/' +
+                                '*;q=0.8',
+                        'Accept-Encoding': 'gzip, deflate, br',
+                        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+                        'Cache-Control': 'max-age=0',
+                        'Connection': 'keep-alive',
+                        'Host': 'www.imdb.com',
+                        'Upgrade-Insecure-Requests': '1',
+                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, lik' +
+                                'e Gecko) Chrome/68.0.3440.106 Safari/537.36'
+                    },
+                    gzip: true
+                });
+            
+                let $ = cheerio.load(response)
+            
+                let title = $('div[class="title_wrapper"] > h1').text().trim()
+                let rating = $('div[class="ratingValue"] > strong > span').text()
+                let poster = $('div[class="poster"] > a > img').attr('src')
+                let totalRatings = $('div[class="imdbRating"] > a').text()
+                let releaseDate = $('a[title="See more release dates"]').text().trim()
+                $('div[class="title_wrapper"] a[href^="/search/title?"]').each((i, el) => {
+                   let genre = $(el).text()
+                   genres.push(genre)
+                })
+    
+                console.log(`
+                   Title: ${title}
+                   Rating: ${rating}
+                   Poster: ${poster}
+                   totalRatings: ${totalRatings}
+                   release Date: ${releaseDate}
+                   Genres: ${genres}
+                `)
+            }
+        }
+    }
+})()
+
+module.exports = Fx
